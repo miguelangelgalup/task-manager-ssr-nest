fbb56277f1d57a15f246a04a99f010489bb44ee9
miguelangeldch <miguelangel.carvallo@galup.com.ve>
2025-10-21 13:37:57 -0400

    prueba tecnica

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './interfaces/tasks.interfaces';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private readonly tasksService: TasksService) {}
+    constructor(private readonly tasksService: TasksService) { }
 
     @Get()
     @HttpCode(HttpStatus.OK)
@@ -11,5 +12,11 @@ export class TasksController {
         return this.tasksService.find();
     }
 
+    @Post('/create-task')
+    @HttpCode(HttpStatus.CREATED)
+    public createTask(@Body() data: Task) {
+
+        return this.tasksService.create({ task: data })
+    }
     // TODO: createTask, updateTask, deleteTask
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,12 +3,16 @@ import { Task } from './interfaces/tasks.interfaces';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
+    private tasks: Task[] = [{ id: 1, title: "Tarea 1", description: "cualquiera", completed: true, priority: "low" }];
     private idCounter = 1;
 
-    constructor () {}
+    constructor() { }
 
     public async find(): Promise<Task[]> {
         return this.tasks;
+    }
+
+    public async create({ task }: { task: Task }) {
+        return this.tasks.push(task)
     }
 }
